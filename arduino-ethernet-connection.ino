95a8399ca4ae328d42aaf6b6a81be449e7177f0b
JeanJuba <jeanwsj96@hotmail.com>
2019-09-02 19:07:32 -0300

    Change POST data array to match the database fields

--- arduino-ethernet-connection.ino
+++ arduino-ethernet-connection.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <Ethernet.h>
+
+
 #define TARGET_IP "192.168.0.200"
 #define TARGET_PORT 3000
 
-
-char PostData[]="{\"Humidity\":\"4\",\"TemperatureC\":\"452\",\"TemperatureF\":\"345\",\"HeatIndexC\":\"53\",\"HeatIndexF\":\"454\",\"Max\":\"433\",\"Min\":\"375\"}";
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x83, 0x9D };
 byte ip[] = { 192, 168, 0, 010 }; // Deve ser único na rede local
 byte gateway[] = { 192, 168, 0, 1 }; // Deixei isso para consultas no caso de você precisar deles
@@ -26,10 +26,12 @@ void loop() {
     Serial.println("Conectado"); //Isso não é necessário, apenas para o gabarito
     Serial.println();
 
+    char PostData[] = "{\"latitude\":\"4\",\"longitude\":\"452\",\"temperature\":\"345\"}";
+   
     client.println("POST /coordinates/ HTTP/1.1");
-    client.println("Host: 192.168.0.200:3000");
+    client.println("Host: " + String(TARGET_IP) + ":" + String(TARGET_PORT));
     client.println("User-Agent: arduino/1.8.9");
-    
+
     client.println("Content-Type:  application/json");
     client.print("Content-Length: ");
     client.println(strlen(PostData));
@@ -38,8 +40,8 @@ void loop() {
     client.println(PostData);
     client.stop();    // Closing connection to server
   } else {
-    Serial.println("Connection Failed."); // Mais uma vez a série é para o gabarito.
+    Serial.println("Connection Failed.");
     Serial.println();
   }
-  delay(5000); // este atraso foi para o meu teste para enviar um número a cada 5 segundos
+  delay(5000);
 }
