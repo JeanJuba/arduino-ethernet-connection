4e53e8ba2193f6a2fc385c3d9f26ada30a9c4b90
JeanJuba <jeanwsj96@hotmail.com>
2019-09-03 20:58:00 -0300

    Add non-blocking DHT library; Add working POST request with temperature and humidity

--- arduino-ethernet-connection.ino
+++ arduino-ethernet-connection.ino
@@ -1,86 +1,80 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h> //INCLUSÃO DE BIBLIOTECA
-#include <DHT_U.h>
-#include <string.h>
+#include <dht_nonblocking.h>
 
 #define TARGET_IP "192.168.0.200"
 #define TARGET_PORT 3000
+#define DHT_SENSOR_TYPE DHT_TYPE_22
 
-#define DHTPIN 53 //PINO DIGITAL UTILIZADO PELO DHT22
-#define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
+static const int DHT_SENSOR_PIN = 53;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x83, 0x9D };
 byte ip[] = { 192, 168, 0, 010 }; // Deve ser único na rede local
 byte gateway[] = { 192, 168, 0, 1 }; // Deixei isso para consultas no caso de você precisar deles
 byte subnet[] = { 255, 255, 255, 0 }; // Deixei isso para consultas no caso de você precisar deles
 
-DHT_Unified dht(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
 EthernetServer server(80);
 EthernetClient client;
-uint32_t delayMS;
+
 
 void setup()
 {
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.begin(9600); //Apenas para a saída para que possamos ver se o seu trabalho
-  dht.begin();
-  delay(1000);
-
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);           // imprime os detalhes do Sensor de Temperatura
-  Serial.println("------------------------------------");
-  Serial.println("Temperatura");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Valor max:    "); Serial.print(sensor.max_value); Serial.println(" *C");
-  Serial.print  ("Valor min:    "); Serial.print(sensor.min_value); Serial.println(" *C");
-  Serial.print  ("Resolucao:   "); Serial.print(sensor.resolution); Serial.println(" *C");
-  Serial.println("------------------------------------");
-
-  dht.humidity().getSensor(&sensor);            // imprime os detalhes do Sensor de Umidade
-  Serial.println("------------------------------------");
-  Serial.println("Umidade");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Valor max:    "); Serial.print(sensor.max_value); Serial.println("%");
-  Serial.print  ("Valor min:    "); Serial.print(sensor.min_value); Serial.println("%");
-  Serial.print  ("Resolucao:   "); Serial.print(sensor.resolution); Serial.println("%");
-  Serial.println("------------------------------------");
-  Serial.println();
-  Serial.println();
-  delayMS = sensor.min_delay / 1000;            // define o atraso entre as leituras
-  delay(1000);
 }
 
+
+
+/*
+   Poll for a measurement, keeping the state machine alive.  Returns
+   true if a measurement is available.
+*/
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if ( millis( ) - measurement_timestamp > 4000ul )
+  {
+    if ( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return ( true );
+    }
+  }
+
+  return ( false );
+}
+
+
+
 void loop() {
-
-  sensors_event_t event;                        // inicializa o evento da Temperatura
-  dht.temperature().getEvent(&event);           // faz a leitura da Temperatura
-
-  if (isnan(event.temperature)) {
-    Serial.println("Erro na leitura da Temperatura!");
-  } else {
-    Serial.print("Temperatura: ");              // imprime a Temperatura
-    Serial.print(event.temperature);
-    Serial.println(" *C");
-  }
-  dht.humidity().getEvent(&event);              // faz a leitura de umidade
-  if (isnan(event.relative_humidity)) {
-    Serial.println("Erro na leitura da Umidade!");
-  } else  {
-    Serial.print("Umidade: ");                  // imprime a Umidade
-    Serial.print(event.relative_humidity);
-    Serial.println("%");
-  }
 
   if (client.connect(TARGET_IP, TARGET_PORT))  {
 
     Serial.println("Conectado"); //Isso não é necessário, apenas para o gabarito
     Serial.println();
 
-    String values = "{\"latitude\":\"4\",\"longitude\":\"452\",\"temperature\":\"" + String(event.temperature) + "\"}";
-    //String values = "{\"latitude\":\"4\",\"longitude\":\"452\",\"temperature\":\"" + String(event.temperature) + "\",\"humidity\":\"" + String(event.relative_humidity) + "\"}";
+    float temperature;
+    float humidity;
+
+    /* Measure temperature and humidity.  If the functions returns
+       true, then a measurement is available. */
+
+    while (measure_environment( &temperature, &humidity ) == false) {
+      Serial.println("Tentando...");
+    }
+
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+
+    //String values = "{\"latitude\":\"4\",\"longitude\":\"452\",\"temperature\":\"" + String(humidity) + "\"}";
+    String values = "{\"latitude\":\"4\",\"longitude\":\"452\",\"temperature\":\"" + String(temperature) + "\",\"humidity\":\"" + String(humidity) + "\"}";
 
     int str_len = values.length() + 1;
 
@@ -101,9 +95,11 @@ void loop() {
     client.println();
     client.println(char_array);
     client.stop();    // Closing connection to server
+
+
   } else {
     Serial.println("Connection Failed.");
     Serial.println();
   }
-  delay(5000);
+  delay(10000);
 }
